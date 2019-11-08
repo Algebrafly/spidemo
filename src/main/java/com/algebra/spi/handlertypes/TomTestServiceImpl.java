e0cb50267681409ffb0eb3e722f75bd961bb56ae
lilong <lilong@zhongruigroup.com>
2019-11-08 16:27:19 +0800

    servlet 3.x 入门

--- src/main/java/com/algebra/spi/handlertypes/TomTestServiceImpl.java
+++ src/main/java/com/algebra/spi/handlertypes/TomTestServiceImpl.java
@@ -7,7 +7,7 @@ package com.algebra.spi.handlertypes;
  * @date: 2019/11/6 21:50
  * @version: 1.0
  */
-public class TomTestService extends AbsTestService {
+public class TomTestServiceImpl extends AbsTestService {
     @Override
     public String sayHello(String name) {
         System.out.println("Tom ------------>>> just for test!!!");
